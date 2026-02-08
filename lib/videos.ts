743805a769f3e480afebd1e1ad51f0aa4bea62fa
User <user@example.com>
2026-02-08 19:28:39 +0000

    Update Featured Videos: Add 3 more videos (5oOWZ0GsTcQ, PaH4REaFQFk, A88N-1bWGxo)

--- lib/videos.ts
+++ lib/videos.ts
@@ -1,7 +1,10 @@
 export const FEATURED_VIDEO_IDS = [
-    '7B5vigbQ-Ek', // New Request
-    'vOsAJF5Eo4w', // New Request
-    'D-Ci5kQvVvI', // New Request
+    '5oOWZ0GsTcQ', // New Request 2
+    'PaH4REaFQFk', // New Request 2
+    'A88N-1bWGxo', // New Request 2
+    '7B5vigbQ-Ek',
+    'vOsAJF5Eo4w',
+    'D-Ci5kQvVvI',
     'cZW3Ouwywag',
     'XhuMt6HlKG4',
     'eASV6ZAv11I',

36ac6e9b078b6cc0be6da3bebdf40fb966cae5ba
User <user@example.com>
2026-02-08 19:27:17 +0000

    Update Featured Videos: Add specific IDs requested by user (7B5vigbQ-Ek, vOsAJF5Eo4w, D-Ci5kQvVvI)

--- lib/videos.ts
+++ lib/videos.ts
@@ -1,9 +1,12 @@
 export const FEATURED_VIDEO_IDS = [
+    '7B5vigbQ-Ek', // New Request
+    'vOsAJF5Eo4w', // New Request
+    'D-Ci5kQvVvI', // New Request
     'cZW3Ouwywag',
     'XhuMt6HlKG4',
     'eASV6ZAv11I',
     'JMkW4jkQ1NY',
     '8L6RA87Rmdk',
     '2LRL6ZKxNjA',
-    'k8qwRIDanTs' // New video added
+    'k8qwRIDanTs'
 ];
